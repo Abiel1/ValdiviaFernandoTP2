f9bf3b28eff0f264d2ef55d52974e4f6a98b253f
Fernando Valdivia <fvhbol@hotmail.com>
2015-02-13 23:54:18 -0500

    Primier fichier , Calcul math

---
+++ CalculMaths/CalculMaths/DeuxNombres.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculMaths
+{
+    class DeuxNombres
+    {
+        //Attributs
+        private int nb1;
+        private int nb2;
+
+        //Constructeur
+        public DeuxNombres(int nb1, int nb2)
+        {
+            this.nb1 = nb1;
+            this.nb2 = nb2;
+        }
+
+        // Méthodes
+        public int Somme()
+        {
+            return nb1+nb2;
+        }
+        public int Produit()
+        {
+            return nb1 * nb2;
+        }
+        public int PGCD()
+        {
+            int reste = nb1 % nb2;
+            while (reste !=0)
+            {
+               nb1=nb2;
+               nb2=reste;
+               reste=nb1 % nb2;
+            }
+            return nb2;
+        }
+        public int PPCM() // non encore implémentée
+        {
+            return 1;
+        }
+    }
+}
---
+++ CalculMaths/CalculMaths/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculMaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DeuxNombres mesDeuxNombres = new DeuxNombres(45, 75);
+            Console.WriteLine("La somme est : " + mesDeuxNombres.Somme());
+            Console.WriteLine("Le produit est : " + mesDeuxNombres.Produit());
+            Console.WriteLine("Le PGCD est : " + mesDeuxNombres.PGCD());
+            Console.WriteLine("Le PPCM est : " + mesDeuxNombres.PPCM());
+            Console.WriteLine("Appuyez sur une touche pour continuer ...");
+            Console.ReadKey();
+        }
+    }
+}
